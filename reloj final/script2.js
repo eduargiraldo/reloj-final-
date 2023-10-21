8b83558be3add6f1bca96b6f4db621ebf8e49cb1
eduargiraldo <139786773+eduargiraldo@users.noreply.github.com>
2023-10-21 11:46:18 -0500

    Add files via upload

---
+++ reloj final/script2.js
@@ -0,0 +1,21 @@
+function calcularTiempo(){
+    let tiempo= new Date();
+
+    let hora = tiempo.getHours();
+    let minuto = tiempo.getMinutes();
+    let segundo = tiempo.getSeconds();
+
+    hora = hora < 10 ? "0" + hora : hora;
+    minuto = minuto < 10? "0" + minuto: minuto;
+    segundo = segundo < 10? "0" + segundo: segundo;
+
+    let ampm = hora >= 12 ? 'PM' : 'AM';
+    hora = hora % 12;
+    hora = hora ? hora : 12;
+
+    let pantallaReloj = hora + ":" + minuto + ":" + segundo + " " + ampm;
+    let relojdigital = document.querySelector(".reloj");
+    relojdigital.innerHTML = pantallaReloj;
+}
+
+setInterval(calcularTiempo, 1000);
